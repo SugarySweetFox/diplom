76bf76b0ccde1ea9ce16b77cac7fc0b077afca8d
Masha <marialytcovskaia>
2024-03-29 21:04:58 +0300

    egee

---
+++ react-diplom/src/components/ChatPage/index.jsx
@@ -0,0 +1,20 @@
+import  classes from "./index.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Preloader from "../Preloader";
+
+
+
+const ChatPage=()=>{
+    return <>
+        <div className={classes.block_brown}>
+            <div className={classes.top}>
+            
+            </div>
+            <div className={classes.block_brown}>
+            
+            </div>
+        </div>
+    </>
+}
+export default ChatPage;
