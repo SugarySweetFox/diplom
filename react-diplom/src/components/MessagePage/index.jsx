c628b52f069d6cb6687d21b3adfc9f6f03ddafde
Masha <marialytcovskaia>
2024-03-29 18:20:17 +0300

    chat

--- react-diplom/src/components/MessagePage/index.jsx
+++ react-diplom/src/components/MessagePage/index.jsx
@@ -11,7 +11,36 @@ const MessagePage=()=>{
             <h3 className={classes.center}>Мои сообщения</h3>
             <div className={classes.message}>
                 <div className={classes.block}>
-                    
+                    <div className={classes.block_top}>
+                        <img src="" alt="" className={classes.img_o} />
+                        <div className={classes.block_text}>
+                            <div className={classes.top_text}>
+                                <h5>Имя</h5>
+                                <h6>12:04</h6>
+                            </div>
+                            <h6>Какое-то сообщение, о чем-то</h6>
+                        </div>
+                    </div>
+                    <div className={classes.line}>
+
+                    </div>
+                </div>
+
+
+                <div className={classes.block}>
+                    <div className={classes.block_top}>
+                        <img src="" alt="" className={classes.img_o} />
+                        <div className={classes.block_text}>
+                            <div className={classes.top_text}>
+                                <h5>Имя</h5>
+                                <h6>12:04</h6>
+                            </div>
+                            <h6>Какое-то сообщение, о чем-то</h6>
+                        </div>
+                    </div>
+                    <div className={classes.line}>
+
+                    </div>
                 </div>
             </div>
         </div>
